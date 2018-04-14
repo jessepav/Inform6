fd520f4e127042f27a02d425b2f2ed19c3186df6
Jesse Pavel <jpavel@alum.mit.edu>
2018-04-14 01:21:44 -0400

    Update build.bat to use parallel make

--- build.bat
+++ build.bat
@@ -1,1 +1,14 @@
-mingw32-make -f Makefile-compiler-mingw32 inform6.exe
+@echo off
+
+setlocal EnableExtensions EnableDelayedExpansion
+
+if "%1" == "" (
+	set TARGET=inform6.exe
+) else (
+	set TARGET=%1
+)	
+
+mingw32-make -j 4 -f Makefile-compiler-mingw32 %TARGET%
+
+endlocal
+
